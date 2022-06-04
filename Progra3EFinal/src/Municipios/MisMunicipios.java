235ae8f020917d435f28e29ac5ddd71dfa72ec4d
Wcastaneda-linares <79226276+Wcastaneda-linares@users.noreply.github.com>
2022-06-04 00:49:00 -0600

    FinalProgra

---
+++ Progra3EFinal/src/Municipios/MisMunicipios.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Municipios;
+
+/**
+ *
+ * @author Wcastañeda
+ */
+public class MisMunicipios {
+    
+    public String jutiapaMunicipios(char cr){
+           String Municipios = "";
+        switch (cr) {
+            case 'a':
+                Municipios = "Jutiapa";
+                break;
+            case 'b':
+                Municipios = "El Progreso";
+                break;
+            case 'c':
+                Municipios = "Santa Catarina Mita";
+                break;
+            case 'd':
+                Municipios = "Agua Blanca";
+                break;
+            case 'e':
+                Municipios = "Asunción Mita";
+                break;
+            case 'f':
+                Municipios = "Yupiltepeque";
+                break;
+            case 'g':
+                Municipios = "Atescatempa";
+                break;
+            case 'h':
+                Municipios = "Jerez";
+                break;
+            case 'i':
+                Municipios = "El Adelanto";
+                break;
+            case 'j':
+                Municipios = "Zapotitlán";
+                break;
+            case 'k':
+                Municipios = "Comapa";
+                break;
+            case 'l':
+                Municipios = "Jalpatagua";
+                break;
+            case 'm':
+                Municipios = "Conguaco";
+                break;
+            case 'n':
+                Municipios = "Moyuta";
+                break;
+            case 'o':
+                Municipios = "Pasaco";
+                break;
+            case 'p':
+                Municipios = "San José Acatempa";
+                break;
+            case 'q':
+                Municipios = "Quezada";
+                break;
+        }
+        
+        return Municipios;
+        
+    }
+    
+ 
+}
---
+++ Progra3EFinal/src/Municipios/idMunicipio.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Municipios;
+
+/**
+ *
+ * @author Wcastañeda
+ */
+public class idMunicipio {
+
+    public char MunicipiosJutiapa(String s) {
+        char id = '&';
+        switch (s) {
+            case "Jutiapa":
+                id = 'a';
+                break;
+            case "El Progreso":
+                id = 'b';
+                break;
+            case "Santa Catarina Mita":
+                id = 'c';
+                break;
+            case "Agua Blanca":
+                id = 'd';
+                break;
+            case "Asunción Mita":
+                id = 'e';
+                break;
+            case "Yupiltepeque":
+                id = 'f';
+                break;
+            case "Atescatempa":
+                id = 'g';
+                break;
+            case "Jerez":
+                id = 'h';
+                break;
+            case "El Adelanto":
+                id = 'i';
+                break;
+            case "Zapotitlán":
+                id = 'j';
+                break;
+            case "Comapa":
+                id = 'k';
+                break;
+            case "Jalpatagua":
+                id = 'l';
+                break;
+            case "Conguaco":
+                id = 'm';
+                break;
+            case "Moyuta":
+                id = 'n';
+                break;
+            case "Pasaco":
+                id = 'o';
+                break;
+            case "San José Acatempa":
+                id = 'p';
+                break;
+            case "Quezada":
+                id = 'g';
+                break;
+        }
+
+        return id;
+
+    }
+}
